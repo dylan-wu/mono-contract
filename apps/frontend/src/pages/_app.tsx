3f3f36eb1b1377cf2a43bc137019ed904773903b
Dylan-Wu <dylanwu@live.com>
2024-03-20 13:45:29 -0500

    Somewhat fixed FOUC

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -1,16 +1,28 @@
-import type { AppType } from "next/app";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 import { trpc } from "../utils/trpc";
 import { MantineProvider } from "@mantine/core";
 import { ClerkProvider } from "@clerk/nextjs";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return (
-    <ClerkProvider {...pageProps}>
-      <MantineProvider>
-        <Component {...pageProps} />
-      </MantineProvider>
-    </ClerkProvider>
-  );
-};
+function App(props: AppProps) {
+  const { Component, pageProps } = props;
 
-export default trpc.withTRPC(MyApp);
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ClerkProvider {...pageProps}>
+        <MantineProvider withGlobalStyles withNormalizeCSS>
+          <Component {...pageProps} />
+        </MantineProvider>
+      </ClerkProvider>
+    </>
+  );
+}
+
+export default trpc.withTRPC(App);
